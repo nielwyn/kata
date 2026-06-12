cd35233657aad5ecc0c33c5343580bb5d8141c04
nielwyn <nghoangnghiabt@gmail.com>
2026-06-13 04:33:40 +0700

    lc88: merge sorted array

---
+++ leetcode/88-merge-sorted-array/solution.go
@@ -0,0 +1,40 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing
+// order, and two integers m and n, representing the number of elements in nums1
+// and nums2 respectively.
+//
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//
+// The final sorted array should not be returned by the function, but instead
+// be stored inside the array nums1. To accommodate this, nums1 has a length of
+// m + n, where the first m elements denote the elements that should be merged,
+// and the last n elements are set to 0 and should be ignored. nums2 has a length
+// of n.
+//
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+
+package mergesortedarray
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	i := m - 1
+	j := n - 1
+	p := m + n - 1
+
+	for i >= 0 && j >= 0 && p >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[p] = nums1[i]
+			i--
+		} else {
+			nums1[p] = nums2[j]
+			j--
+		}
+		p--
+	}
+
+	// nums1 loop exit, handle remaining nums2
+	for j >= 0 {
+		nums1[p] = nums2[j]
+		j--
+		p--
+	}
+}
